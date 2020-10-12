0700ceb81c7a36e6ec6ef3998f26cdcc1bdbc3b0
vjkarthigaa <vjkarthigaa@gmail.com>
2020-10-12 11:26:20 +0530

    Updates

---
+++ precache-manifest.ea9a4f59505cc5b890bd90522544d3cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53374ee78a1bfdbda2b5cf320b8f475",
+    "url": "/Todo-list-app_Karthigaa/index.html"
+  },
+  {
+    "revision": "29e6da1eb52d91380b49",
+    "url": "/Todo-list-app_Karthigaa/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "b3fa0dd4cd4902f2b72d",
+    "url": "/Todo-list-app_Karthigaa/static/css/main.c213efd2.chunk.css"
+  },
+  {
+    "revision": "29e6da1eb52d91380b49",
+    "url": "/Todo-list-app_Karthigaa/static/js/2.5c58a866.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-list-app_Karthigaa/static/js/2.5c58a866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3fa0dd4cd4902f2b72d",
+    "url": "/Todo-list-app_Karthigaa/static/js/main.2cc3f3b9.chunk.js"
+  },
+  {
+    "revision": "d70a6b7fea8a9d8eca5d",
+    "url": "/Todo-list-app_Karthigaa/static/js/runtime-main.1ab86459.js"
+  }
+]);
